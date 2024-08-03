88e5f468cb3ab081dc7be73002f1c5802e1c56c7
aleshaujay <aleshaujay@icloud.com>
2024-08-04 03:43:06 +0700

    I implemented the Palette application in which I created 3 sliders for red, green, blue and one canvas on which you can see different shades of GRB

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    @IBOutlet var paletteView: UIView!
 
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        paletteView.layer.cornerRadius = 20
+        
+        paletteView.backgroundColor = UIColor( // здесь Xcode пытался помочь, но что-то предлагаемый им код не полетел 🤷‍♂️
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1 // этот параметр я хотел задать только в Storyboard, но его предложил Xcode и я его решил оставить тут в коде
+        )
+        
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        paletteView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    // тут я заподозрил повторение кода и задумался, что можно это как-то сократить, но так и не понял как 🤷‍♂️
+     @IBAction func greenSliderAction() {
+        paletteView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+         
+         redLabel.text = String(format: "%.2f", redSlider.value)
+         greenLabel.text = String(format: "%.2f", greenSlider.value)
+         blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    @IBAction func blueSliderAction() {
+        paletteView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
 }
 
